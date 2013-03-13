1e7f8b942d57da03b4560502418a22403718470e
Haacked <haacked@gmail.com>
2013-03-12 20:44:23 -0700

    Initial commit
    I don't even know if the tests run. I should update my tests.

---
+++ TextWrapper/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TextWrapper
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int lineLength = 80;
+            if (args.Length > 1) 
+            {
+                lineLength = Convert.ToInt32(args[1]);
+            }
+
+            var path = Path.GetFullPath(args[0]);
+
+            IEnumerable<FileInfo> files;
+            if (File.Exists(path))
+            {
+                files = new[] { new FileInfo(path) };
+            }
+            else { 
+                var dir = new DirectoryInfo(Path.GetFullPath(args[0]));
+                files = dir.GetFiles("*.txt", SearchOption.TopDirectoryOnly).Union(dir.GetFiles("*.md", SearchOption.TopDirectoryOnly));
+            }
+
+            foreach (var file in files)
+            {
+                File.WriteAllText(file.FullName + ".out", File.ReadAllText(file.FullName).Wrap(80));
+            }
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string Wrap(this string text, int maxLength = 72)
+        {
+            if (text == null) throw new ArgumentNullException("text");
+            if (text.Length == 0) return string.Empty;
+
+            var lines = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            var fittedLines = lines.SelectMany(l => l.LinesWithinLength(maxLength)).ToArray();
+            return String.Join(Environment.NewLine, fittedLines);
+        }
+
+        public static IEnumerable<string> LinesWithinLength(this string text, int length)
+        {
+            if (text.Length == 0) 
+                yield return String.Empty;
+            else if (text.Length <= length)
+                yield return text;
+            else
+            {
+                int position = length;
+                char c = text[position];
+                if (c != ' ')
+                {
+                    while (c != ' ' && position > -1)
+                    {
+                        position--;
+                        c = text[position];
+                    }
+                    position++; // Keep the last space
+                }
+                yield return text.Substring(0, position);
+                foreach (var fittedLine in text.Substring(position).TrimStart().LinesWithinLength(length))
+                    yield return fittedLine;
+            }
+        }
+
+        public static string Wrap2(this string text, int maxLength = 72)
+        {
+            if (text == null) throw new ArgumentNullException("text");
+            if (text.Length == 0) return string.Empty;
+
+            var sb = new StringBuilder();
+            foreach (var unwrappedLine in text.Split(new[] { Environment.NewLine }, StringSplitOptions.None))
+            {
+                var line = new StringBuilder();
+                foreach (var word in unwrappedLine.Split(' '))
+                {
+                    var needsLeadingSpace = line.Length > 0;
+
+                    var extraLength = (needsLeadingSpace ? 1 : 0) + word.Length;
+                    if (line.Length + extraLength > maxLength)
+                    {
+                        sb.AppendLine(line.ToString());
+                        line.Clear();
+                        needsLeadingSpace = false;
+                    }
+
+                    if (needsLeadingSpace)
+                        line.Append(" ");
+
+                    line.Append(word);
+                }
+
+                sb.AppendLine(line.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ UnitTests/TextWrapperTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Linq;
+using TextWrapper;
+using Xunit;
+using Xunit.Extensions;
+
+namespace UnitTests
+{
+    public class TextWrapperTests
+    {
+        public class TheWrapMethod
+        {
+            [Theory]
+            [InlineData(@"This is not indented.
+    This is indented
+    So is this.
+But this is not")]
+            [InlineData("")]
+            [InlineData("Blah blah blah")]
+            [InlineData(@"Blah blah
+
+blah blah")]
+            public void DoesNotChangeTextThatFits(string text)
+            {
+                Assert.Equal(text, text.Wrap());
+            }
+
+            [Theory]
+            [InlineData("This is just a bunch of text. This is more text. This is",
+                @"This is just a
+bunch of text.
+This is more 
+text. This is")]
+            public void InsertsWrappingForTooLongLines(string text, string expected)
+            {
+                Assert.Equal(expected, text.Wrap(14));
+            }
+        }
+
+        public class TheLinesWithinLengthMethod
+        {
+            [Theory]
+            [InlineData("This is way too long", 80)]
+            [InlineData("This is way too long", 20)]
+            [InlineData("    This is way too long", 24)]
+            [InlineData("", 20)]
+            public void ReturnsLinesThatAreSmallerThanOrEqualToLength(string text, int length)
+            {
+                var lines = text.LinesWithinLength(length).ToArray();
+                Assert.Equal(text, lines[0]);
+            }
+
+            [Theory]
+            [InlineData(@"This is way too long", 12, "This is way ", "too long")]
+            [InlineData(@"This is way too long", 11, "This is way", "too long")]
+            public void ReturnsLinesFormattedToFit(string text, int maxLength, string firstLine, string secondLine)
+            {
+                var lines = text.LinesWithinLength(maxLength).ToArray();
+                Assert.Equal(firstLine, lines[0]);
+                Assert.Equal(secondLine, lines[1]);
+            }
+
+            [Fact]
+            public void ReturnsWayTooLongLinesFormattedToFit()
+            {
+                const string text = @"This is way too long";
+                var lines = text.LinesWithinLength(8).ToArray();
+                Assert.Equal("This is ", lines[0]);
+                Assert.Equal("way too ", lines[1]);
+                Assert.Equal("long", lines[2]);
+            }
+
+            [Fact]
+            public void ReturnsWayTooLongLinesWhenLengthHitsMiddleOfStringFormattedToFit()
+            {
+                const string text = @"This is way too long";
+                var lines = text.LinesWithinLength(9).ToArray();
+                Assert.Equal("This is ", lines[0]);
+                Assert.Equal("way too ", lines[1]);
+                Assert.Equal("long", lines[2]);
+            }
+
+            [Fact]
+            public void KeepsExtraSpaces()
+            {
+                const string text = @"This is  way too long";
+                var lines = text.LinesWithinLength(10).ToArray();
+                Assert.Equal("This is  ", lines[0]);
+                Assert.Equal("way too ", lines[1]);
+                Assert.Equal("long", lines[2]);
+            }
+
+            [Fact]
+            public void ReturnsEmptyStringForEmptyString()
+            {
+                const string text = @"";
+                var lines = text.LinesWithinLength(10).ToArray();
+                Assert.Equal("", lines[0]);
+            }
+        }
+    }
+}
